e98433aad3ac5784ce0eee13b9e50a23d0e7ef53
ASUS\USER <xiiruru@gmail.com>
2018-04-24 04:21:59 +0800

    CRUD Updated

--- web-app/src/app/assets/asset-detail/asset-detail.component.ts
+++ web-app/src/app/assets/asset-detail/asset-detail.component.ts
@@ -1,35 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Asset } from '../shared/asset';
 import { AssetService } from '../shared/asset.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-asset-detail',
   templateUrl: './asset-detail.component.html',
-  styleUrls: ['./asset-detail.component.scss'],
-  providers: [AssetService]
+  styleUrls: ['./asset-detail.component.scss']
 })
 
 export class AssetDetailComponent implements OnInit {
 
-  assets:Asset[] = [];
-
-  constructor(private assetService : AssetService) { }
+  constructor(private assetService : AssetService, private toastr : ToastrService) { }
 
   ngOnInit() {
   	//do request and get asset's record
   	this.assetService.getAssets().subscribe(res =>{
-
-  		this.assets = res;
+  		this.assetService.assets = res;
   		console.log(res);
-
   	}, err => {
-
   		console.log(err);
   	})
   }
 
+  //Show edit
   showForEdit(asset: Asset) {
     this.assetService.selectedAsset = Object.assign({},asset);
     console.log(this.assetService.selectedAsset);
   }
+
+  //On delete
+  onDelete(id : number) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+    this.assetService.deleteAsset(id).subscribe(res => {
+      this.toastr.success('Record Succesfully deleted!','Asset Register');
+      console.log(res);
+
+      //Retrieving new data from datasource
+      this.assetService.getAssets().subscribe(res=> { 
+        this.assetService.assets = res;
+        console.log(res);        
+      },
+        err=> {
+          console.log(err);
+        });
+
+    }, err => {
+      this.toastr.error('There is an error occured!','Asset Register');
+      console.log(err);
+    });
+  }
+ };
+
 }
--- web-app/src/app/assets/asset/asset.component.ts
+++ web-app/src/app/assets/asset/asset.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-
 
 import { AssetService } from '../shared/asset.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
   selector: 'app-asset',
   templateUrl: './asset.component.html',
-  styleUrls: ['./asset.component.scss'],
-  providers: [AssetService]
+  styleUrls: ['./asset.component.scss']
 })
 
 export class AssetComponent implements OnInit {
 
 
-  constructor(private assetService:AssetService, private toastr : ToastrService) { }
+  constructor(private assetService : AssetService, private toastr : ToastrService) { }
+
 
   ngOnInit() {
   	 this.resetForm();
@@ -35,18 +34,45 @@ export class AssetComponent implements OnInit {
   		description : '',
   		remark: ''
   	}
-  	
   }
 
   onSubmit(form : NgForm) {
-  	this.assetService.insertAsset(this.assetService.selectedAsset).subscribe(
-  		res => {
-  			this.resetForm(form);
-  			this.toastr.success('New Record Succesfully Added!','Asset Register');
-  		},
-  		err => {
-  			this.toastr.error('There is an error occured!','Asset Register');
-  		});
+    if(form.value.id == null) {
+      this.assetService.insertAsset(this.assetService.selectedAsset).subscribe(
+      res => {
+        this.resetForm(form);
+        this.assetService.getAssets().subscribe(res =>{
+
+          this.assetService.assets = res;
+          console.log(res);
+
+          }, err => {
+
+          console.log(err);
+        });
+
+        this.toastr.success('New Record Succesfully Added!','Asset Register');
+      },
+      err => {
+        this.toastr.error('There is an error occured!','Asset Register');
+      });
+    }
+    else {
+      this.assetService.updateAsset(this.assetService.selectedAsset.id, this.assetService.selectedAsset).subscribe(
+      res => {
+        this.resetForm(form);
+        this.assetService.getAssets().subscribe(res =>{
+          this.assetService.assets = res;
+          console.log(res);
+          }, err => {
+          console.log(err);
+        });
+        this.toastr.info('Record Succesfully Updated!','Asset Register');
+      },
+      err => {
+        this.toastr.error('There is an error occured!','Asset Register');
+      });
+    }
   }
 
 }
