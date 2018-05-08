2f13043e15ea79b096938d5ef2def2b48beb5c9b
RaikireHiuduo <chun_heng@hotmail.com>
2018-05-08 10:52:49 +0800

    update the blockchain design

--- blockchain/composer/lib/logic.js
+++ blockchain/composer/lib/logic.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/**
+ * Track the update of an asset from one place to another
+ * @param {org.bit.beems.UpdateBEEMSAsset} updateBEEMSAsset - the BEEMSasset to be processed
+ * @transaction
+ */
+async function doUpdateBEEMSAsset(updateBEEMSAsset) {
+    updateBEEMSAsset.oldBEEMSAssetInfromation.assetContentHash = updateBEEMSAsset.assetContentHash;
+    updateBEEMSAsset.oldBEEMSAssetInfromation.currentGPSLocation = updateBEEMSAsset.currentGPSLocation;
+
+    let assetRegistry = await getAssetRegistry('org.bit.beems.BEEMSAsset');
+    await assetRegistry.update(updateBEEMSAsset.oldBEEMSAssetInfromation);
+}
